a9bbc1c552b3a5993d932998e95f0a2ffa821969
Lius <142863069+noturnachs@users.noreply.github.com>
2024-08-24 17:38:08 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Main from "./main";
 
+const isMaintenance = true;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Main />);
+
+if (isMaintenance) {
+  // Redirect to maintenance page
+  window.location.href = "/maintenance.html";
+} else {
+  root.render(<Main />);
+}
