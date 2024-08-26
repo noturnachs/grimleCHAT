fff700f3f82ae5162a7bcd04b8064a5c61b1373e
Lius <142863069+noturnachs@users.noreply.github.com>
2024-08-26 23:29:29 +0800

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,6 +5,7 @@ import { motion, useSpring, useTransform } from "framer-motion";
 import Announcement from "./Announcement";
 import FingerprintJS from "@fingerprintjs/fingerprintjs"; // Import FingerprintJS
 import FAQ from "./Faq";
+import { FaFlag } from "react-icons/fa";
 
 const SERVER_ORIGIN = process.env.REACT_APP_SERVER_ORIGIN;
 
@@ -147,6 +148,10 @@ function Home() {
     getFingerprint();
   }, [navigate]);
 
+  const handleReportClick = () => {
+    navigate("/report-problems"); // Navigate to the report form route
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     setError("");
@@ -198,194 +203,209 @@ function Home() {
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-[#192734] p-4">
       <Announcement />
-      <div className="flex flex-col space-y-10 justify-center items-center md:flex-row md:space-x-5 md:space-y-0">
-        <div className="bg-[#15202b] p-3 rounded-lg shadow-lg max-w-md w-full md:p-8">
-          <h1 className="text-2xl font-normal mb-6 text-white text-center">
-            Welcome to LeeyosChat
-          </h1>
+      <div className="flex flex-col space-y-10 justify-center items-center md:flex-row md:space-x-5 md:space-y-0 ">
+        <div className="mt-[50vh] iphone-xr:mt-[10vh] md:mt-[20vh] pixel-7:mt-[10vh] s-8+:mt-[30vh] ipad-air:mt-[1vh] surface-duo:mt-[30vh] z-fold:mt-[20vh] nest-hub:mt-[70vh] nest-hubmax:mt-[40vh] def-an:mt-[30vh] ip-se:mt-[50vh]">
+          <div className="bg-[#15202b] p-3 rounded-lg shadow-lg max-w-md w-full md:p-8">
+            <h1 className="text-2xl font-normal mb-6 text-white text-center ">
+              Welcome to LeeyosChat
+            </h1>
 
-          <p className="text-sm text-gray-300 mb-4">
-            Talk with random strangers anywhere and everywhere.
-          </p>
+            <p className="text-sm text-gray-300 mb-4">
+              Talk with random strangers anywhere and everywhere.
+            </p>
 
-          {/* Display user count */}
-          <div className="text-sm text-gray-300 mb-4 text-center">
-            <Counter value={userCount} />
-          </div>
-
-          {showTerms && (
-            <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[#192734] bg-opacity-50 text-gray-300 z-50">
-              <div className="bg-[#15202b] p-6 rounded-lg shadow-lg">
-                <h2 className="text-lg font-semibold mb-4">
-                  LeeyosChat Terms and Conditions
-                </h2>
-                <p>
-                  By using LeeyosChat, you agree to the following terms and
-                  conditions:
-                </p>
-                <ul className="list-disc pl-6 mb-4">
-                  <li>You must be at least 18 years old to use LeeyosChat.</li>
-                  <li>
-                    You are solely responsible for your interactions and the
-                    messages you send on LeeyosChat.
-                  </li>
-                  <li>
-                    Do not send any illegal, harmful, threatening, abusive,
-                    harassing, defamatory, vulgar, obscene, hateful, or
-                    racially, ethnically, or otherwise objectionable messages on
-                    LeeyosChat.
-                  </li>
-                  <li>
-                    Do not impersonate any other person or entity on LeeyosChat.
-                  </li>
-                  <li>
-                    We reserve the right to terminate access to LeeyosChat for
-                    users who violate our community guidelines.
-                  </li>
-                </ul>
-                <p className="font-semibold">Disclaimer of Liability</p>
-                <p>
-                  LeeyosChat is provided on an "as is" basis. We make no
-                  warranties about the operation of LeeyosChat or the
-                  information on it. You agree that your use of LeeyosChat is at
-                  your sole risk.
-                </p>
-                <button
-                  className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4"
-                  onClick={() => setShowTerms(false)}
-                >
-                  I Agree
-                </button>
-              </div>
+            {/* Display user count */}
+            <div className="text-sm text-gray-300 mb-4 text-center">
+              <Counter value={userCount} />
             </div>
-          )}
 
-          <form onSubmit={handleSubmit} className="space-y-4">
-            {error && (
-              <div className="text-red-500 text-sm text-center">{error}</div>
+            {showTerms && (
+              <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[#192734] bg-opacity-50 text-gray-300 z-50">
+                <div className="bg-[#15202b] p-6 rounded-lg shadow-lg">
+                  <h2 className="text-lg font-semibold mb-4">
+                    LeeyosChat Terms and Conditions
+                  </h2>
+                  <p>
+                    By using LeeyosChat, you agree to the following terms and
+                    conditions:
+                  </p>
+                  <ul className="list-disc pl-6 mb-4">
+                    <li>
+                      You must be at least 18 years old to use LeeyosChat.
+                    </li>
+                    <li>
+                      You are solely responsible for your interactions and the
+                      messages you send on LeeyosChat.
+                    </li>
+                    <li>
+                      Do not send any illegal, harmful, threatening, abusive,
+                      harassing, defamatory, vulgar, obscene, hateful, or
+                      racially, ethnically, or otherwise objectionable messages
+                      on LeeyosChat.
+                    </li>
+                    <li>
+                      Do not impersonate any other person or entity on
+                      LeeyosChat.
+                    </li>
+                    <li>
+                      We reserve the right to terminate access to LeeyosChat for
+                      users who violate our community guidelines.
+                    </li>
+                  </ul>
+                  <p className="font-semibold">Disclaimer of Liability</p>
+                  <p>
+                    LeeyosChat is provided on an "as is" basis. We make no
+                    warranties about the operation of LeeyosChat or the
+                    information on it. You agree that your use of LeeyosChat is
+                    at your sole risk.
+                  </p>
+                  <button
+                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4"
+                    onClick={() => setShowTerms(false)}
+                  >
+                    I Agree
+                  </button>
+                </div>
+              </div>
             )}
 
-            <div>
-              <input
-                id="username"
-                value={username}
-                placeholder="What should we call you?"
-                onChange={(e) => setUsername(e.target.value)}
-                required
-                className="bg-[#192734] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-[#fff] cursor-pointer transition w-full"
-                type="text"
-              />
+            <form onSubmit={handleSubmit} className="space-y-4">
+              {error && (
+                <div className="text-red-500 text-sm text-center">{error}</div>
+              )}
 
-              <input
-                id="interest"
-                value={interestInput}
-                placeholder="What are your interests?"
-                onChange={(e) => setInterestInput(e.target.value)}
-                onKeyDown={handleInterestKeyDown} // Add interest on Enter key
-                onBlur={() => {
-                  if (interestInput.trim()) {
-                    setInterest([...interest, interestInput.trim()]);
-                    setInterestInput(""); // Clear the input field after adding the interest
-                  }
-                }} // Add interest when the input loses focus
-                className="bg-[#192734] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-[#fff] cursor-pointer transition w-full mt-2"
-                type="text"
-              />
-              {/* Display the interests as tags with remove buttons */}
-              <div className="flex flex-wrap gap-2 mt-2">
-                {interest.map((item, index) => (
-                  <div
-                    key={index}
-                    className="flex items-center bg-blue-500 text-white px-2 py-1 rounded-lg text-sm"
-                  >
-                    {item}
-                    <button
-                      className="ml-2 text-red-500 hover:text-red-700"
-                      onClick={() => handleRemoveInterest(index)}
-                    >
-                      &times;
-                    </button>
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            {showPasswordInput && (
               <div>
                 <input
-                  id="password"
-                  value={password}
-                  placeholder="Enter admin password"
-                  onChange={(e) => setPassword(e.target.value)}
+                  id="username"
+                  value={username}
+                  placeholder="What should we call you?"
+                  onChange={(e) => setUsername(e.target.value)}
                   required
                   className="bg-[#192734] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-[#fff] cursor-pointer transition w-full"
-                  type="password"
+                  type="text"
                 />
+
+                <input
+                  id="interest"
+                  value={interestInput}
+                  placeholder="What are your interests?"
+                  onChange={(e) => setInterestInput(e.target.value)}
+                  onKeyDown={handleInterestKeyDown} // Add interest on Enter key
+                  onBlur={() => {
+                    if (interestInput.trim()) {
+                      setInterest([...interest, interestInput.trim()]);
+                      setInterestInput(""); // Clear the input field after adding the interest
+                    }
+                  }} // Add interest when the input loses focus
+                  className="bg-[#192734] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-[#fff] cursor-pointer transition w-full mt-2"
+                  type="text"
+                />
+                {/* Display the interests as tags with remove buttons */}
+                <div className="flex flex-wrap gap-2 mt-2">
+                  {interest.map((item, index) => (
+                    <div
+                      key={index}
+                      className="flex items-center bg-blue-500 text-white px-2 py-1 rounded-lg text-sm"
+                    >
+                      {item}
+                      <button
+                        className="ml-2 text-red-500 hover:text-red-700"
+                        onClick={() => handleRemoveInterest(index)}
+                      >
+                        &times;
+                      </button>
+                    </div>
+                  ))}
+                </div>
               </div>
-            )}
 
-            {/* Checkboxes */}
-            <div className="flex flex-col gap-2">
-              <label
-                htmlFor="over18"
-                className="text-white flex items-center text-md"
-              >
-                <input
-                  type="checkbox"
-                  id="over18"
-                  checked={over18}
-                  onChange={() => setOver18(!over18)}
-                  className="mr-2 w-4 h-4"
-                  required
-                />
-                I am over 18 years old
-              </label>
+              {showPasswordInput && (
+                <div>
+                  <input
+                    id="password"
+                    value={password}
+                    placeholder="Enter admin password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="bg-[#192734] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-[#fff] cursor-pointer transition w-full"
+                    type="password"
+                  />
+                </div>
+              )}
 
-              <label
-                htmlFor="agreeTerms"
-                className="text-white flex items-center text-md"
-              >
-                <input
-                  type="checkbox"
-                  id="agreeTerms"
-                  checked={agreeTerms}
-                  onChange={() => setAgreeTerms(!agreeTerms)}
-                  className="mr-2 w-4 h-4"
-                  required
-                />
-                I agree to the&nbsp;
-                <button
-                  type="button"
-                  onClick={() => setShowTerms(true)}
-                  className="underline cursor-pointer"
+              {/* Checkboxes */}
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="over18"
+                  className="text-white flex items-center text-md"
                 >
-                  Terms of Service
-                </button>
-              </label>
-            </div>
+                  <input
+                    type="checkbox"
+                    id="over18"
+                    checked={over18}
+                    onChange={() => setOver18(!over18)}
+                    className="mr-2 w-4 h-4"
+                    required
+                  />
+                  I am over 18 years old
+                </label>
 
-            {!showTerms && (
-              <button
-                type="submit"
-                className="overflow-hidden w-full p-2 h-12 bg-[#325E87] text-white border-none rounded-md text-md font-normal cursor-pointer relative z-10 group flex items-center justify-center"
-              >
-                <span className="absolute inset-0 flex items-center justify-center bg-[#325E87] group-hover:opacity-0 transition-opacity duration-1000">
-                  Are you ready?
-                </span>
-                <span className="absolute w-[200%] h-32 -top-8 -left-1/2 bg-green-200 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-bottom"></span>
-                <span className="absolute w-[200%] h-32 -top-8 -left-1/2 bg-green-400 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-bottom"></span>
-                <span className="absolute w-[200%] h-32 -top-8 -left-1/2 bg-green-600 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-bottom"></span>
-                <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute inset-0 flex items-center justify-center z-10">
-                  Continue
-                </span>
-              </button>
-            )}
-          </form>
+                <label
+                  htmlFor="agreeTerms"
+                  className="text-white flex items-center text-md"
+                >
+                  <input
+                    type="checkbox"
+                    id="agreeTerms"
+                    checked={agreeTerms}
+                    onChange={() => setAgreeTerms(!agreeTerms)}
+                    className="mr-2 w-4 h-4"
+                    required
+                  />
+                  I agree to the&nbsp;
+                  <button
+                    type="button"
+                    onClick={() => setShowTerms(true)}
+                    className="underline cursor-pointer"
+                  >
+                    Terms of Service
+                  </button>
+                </label>
+              </div>
+
+              {!showTerms && (
+                <button
+                  type="submit"
+                  className="overflow-hidden w-full p-2 h-12 bg-[#325E87] text-white border-none rounded-md text-md font-normal cursor-pointer relative z-10 group flex items-center justify-center"
+                >
+                  <span className="absolute inset-0 flex items-center justify-center bg-[#325E87] group-hover:opacity-0 transition-opacity duration-1000">
+                    Are you ready?
+                  </span>
+                  <span className="absolute w-[200%] h-32 -top-8 -left-1/2 bg-green-200 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-bottom"></span>
+                  <span className="absolute w-[200%] h-32 -top-8 -left-1/2 bg-green-400 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-bottom"></span>
+                  <span className="absolute w-[200%] h-32 -top-8 -left-1/2 bg-green-600 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-bottom"></span>
+                  <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute inset-0 flex items-center justify-center z-10">
+                    Continue
+                  </span>
+                </button>
+              )}
+            </form>
+          </div>
         </div>
       </div>
+
       <FAQ />
-      <footer className="absolute bottom-0 w-full text-center py-4 bg-transparent text-white mt-20">
-        <p>
+      <button
+        onClick={handleReportClick}
+        className="flex items-center text-white mt-4 rounded bg-[#d6663a] p-2 md:p-3 md:mt-6" // Adjust padding and margin for larger screens
+      >
+        <FaFlag className="mr-2" /> Report an Issue
+      </button>
+
+      <footer className="w-full text-center py-4 bg-transparent text-white mt-4 mb-0 md:mt-20">
+        {" "}
+        {/* Removed absolute positioning */}
+        <p className="text-sm md:text-base">
           For reports/suggestions, please fill out this&nbsp;
           <a
             href="https://forms.gle/ybVxyE4H9Fz5Lygr9"
