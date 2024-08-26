ab443e6fc446e7b52df422c7b82e649028b8c514
Lius <142863069+noturnachs@users.noreply.github.com>
2024-08-27 01:45:59 +0800

    Create Popup.jsx

---
+++ src/components/Popup.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Popup = ({ message, onClose }) => {
+  return (
+    <div className="fixed top-0 left-1/2 transform -translate-x-1/2 w-[200px] bg-[#384d61] bg-opacity-80 shadow-lg p-4 transition-transform animate-slide-down z-[9999] mt-5 rounded">
+      <button
+        onClick={onClose}
+        className="absolute top-2 right-2 text-red-500 text-lg font-bold"
+      >
+        &times;
+      </button>
+      <h1 className="font-extrabold text-[#ff9927]">Admin</h1>
+      <p className="whitespace-pre-wrap break-words font-bold text-white text-sm">
+        {message}
+      </p>
+    </div>
+  );
+};
+
+export default Popup;
