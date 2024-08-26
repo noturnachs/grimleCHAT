5a2d8d9931a7d8a2a81702bba078ffdcede77049
Lius <142863069+noturnachs@users.noreply.github.com>
2024-08-26 23:29:03 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import ChatRoom from "./chatroom";
 import BanPage from "./components/BanPage";
+import ReportForm from "./components/ReportForm";
 
 function Main() {
   return (
@@ -11,6 +12,7 @@ function Main() {
         <Route path="/" element={<Home />} />
         <Route path="/chat" element={<ChatRoom />} />
         <Route path="/banned" element={<BanPage />} />
+        <Route path="/report-problems" element={<ReportForm />} />
       </Routes>
     </BrowserRouter>
   );
