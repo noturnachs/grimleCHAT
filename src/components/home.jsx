906e75f0fc4acfa6318b3203d87ebfb29c6f6a22
Lius <142863069+noturnachs@users.noreply.github.com>
2024-08-29 06:39:54 +0800

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import socket from "../socket"; // Import the singleton socket instance
@@ -398,6 +399,9 @@ function Home() {
       </div>
 
       <FAQ />
+      <p className="text-sm text-gray-300 text-center mt-4">
+        We do not store any chat information on our server, and because of that, once you switch to another page and go back or even refresh, you won't be able to see your chats anymore. It is expected that when you leave the website, you are leaving the chat. That is most likely the reason why you are not able to see your messages or communicate with your match.
+      </p>
       <button
         onClick={handleReportClick}
         className="flex items-center text-white mt-4 rounded bg-[#d6663a] p-2 md:p-3 md:mt-6" // Adjust padding and margin for larger screens
