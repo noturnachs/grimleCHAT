e80b84b107d8ce1d55a30395ca35a78f15449110
Lius <142863069+noturnachs@users.noreply.github.com>
2024-08-30 16:24:54 +0800

    Update Faq.jsx

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 
 function FAQ() {
-  const [isAnswerVisible, setIsAnswerVisible] = useState(false);
+  // State to track the currently open question
+  const [openQuestion, setOpenQuestion] = useState(null);
 
-  const toggleAnswerVisibility = () => {
-    setIsAnswerVisible(!isAnswerVisible);
+  // Function to toggle the visibility of the answer
+  const toggleAnswerVisibility = (questionIndex) => {
+    // If the clicked question is already open, close it; otherwise, open the clicked question
+    setOpenQuestion(openQuestion === questionIndex ? null : questionIndex);
   };
 
   return (
@@ -17,20 +20,46 @@ function FAQ() {
           {/* Clickable question */}
           <h2
             className="font-bold text-white text-md mb-3 cursor-pointer bg-[#121b22] rounded p-2"
-            onClick={toggleAnswerVisibility}
+            onClick={() => toggleAnswerVisibility(0)} // Pass the index of the question
           >
             Am I matched with people with the same interests?
           </h2>
           {/* Conditionally render the answer with smooth transition */}
           <div
             className={`transition-max-height duration-500 ease-in-out overflow-hidden ${
-              isAnswerVisible ? "max-h-40" : "max-h-0"
+              openQuestion === 0 ? "max-h-40" : "max-h-0"
             }`}
           >
             <p className="text-sm text-justify">
               Our website's logic will try to find people who share the same
               interests as you. If no match is found with the same interests,
               you will be paired with a random person.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-sm text-gray-300">
+        <div className="mb-4">
+          {/* Clickable question */}
+          <h2
+            className="font-bold text-white text-md mb-3 cursor-pointer bg-[#121b22] rounded p-2"
+            onClick={() => toggleAnswerVisibility(1)} // Pass the index of the question
+          >
+            Why can't I read or send messages whenever I came back from
+            switching an app?
+          </h2>
+          {/* Conditionally render the answer with smooth transition */}
+          <div
+            className={`transition-max-height duration-500 ease-in-out overflow-hidden ${
+              openQuestion === 1 ? "max-h-40" : "max-h-0"
+            }`}
+          >
+            <p className="text-sm text-justify">
+              The everything send during the chat are not stored in anyway, so
+              whenever you leave the website everything is gone. That is why you
+              are unable to send or read new messages already. Just find another
+              match.
             </p>
           </div>
         </div>
