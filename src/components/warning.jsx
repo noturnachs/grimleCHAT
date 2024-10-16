d79b5e86684845a746ed41972e12f05d0cedcbea
Lius <142863069+noturnachs@users.noreply.github.com>
2024-10-16 15:48:17 +0800

    Create warning.jsx

---
+++ src/components/warning.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Warning = () => {
+  return (
+    <div className="bg-red-500 text-white p-2 rounded-lg text-center mb-4 text-sm">
+      We take serious actions for inappropriate users. Especially{" "}
+      <span className="font-bold text-black">HORNY</span> users.
+    </div>
+  );
+};
+
+export default Warning;
