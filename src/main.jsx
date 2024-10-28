a5aace3e1b044f7d556c65c35ac0acdc535345b0
Lius <142863069+noturnachs@users.noreply.github.com>
2024-10-28 22:04:05 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/home";
 import ChatRoom from "./chatroom";
 import BanPage from "./components/BanPage";
@@ -14,7 +14,9 @@ function Main() {
         <Route path="/chat" element={<ChatRoom />} />
         <Route path="/banned" element={<BanPage />} />
         <Route path="/report-problems" element={<ReportForm />} />
-        <Route path="*" element={<NotFound />} />{" "}
+        {/* 404 handling */}
+        <Route path="/page-not-found" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/page-not-found" replace />} />
         {/* This catches all unmatched routes */}
       </Routes>
     </BrowserRouter>
